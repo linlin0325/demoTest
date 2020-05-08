65ab4e8225b31562b3416e3d97fa243bf02e3314
linlin <linlin@weconex.com>
2020-05-08 14:02:22 +0800

    第三章 thread api

---
+++ src/main/java/com/demo/thread/chap3/ThreadJoin.java
@@ -0,0 +1,36 @@
+package com.demo.thread.chap3;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ThreadJoin {
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = IntStream.range(1,3)
+                .mapToObj(ThreadJoin::create).collect(Collectors.toList());
+        threads.forEach(Thread::start);
+        for (Thread thread: threads){
+            thread.join();
+        }
+        for(int i = 0; i < 10; i++){
+            System.out.println(Thread.currentThread().getName() + "#" + i);
+            shortSleep();
+        }
+    }
+    private static Thread create(int seq){
+        return new Thread(()->{
+            IntStream.range(0,10).forEach(i->{
+                System.out.println(Thread.currentThread().getName()+"#"+i);
+                shortSleep();
+            });
+        });
+    }
+    private static void shortSleep(){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
